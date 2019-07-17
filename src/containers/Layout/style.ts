147ba25746b6652def62e0b68228ef66f16501f0
Anton Olenin <anton.olenin@btsdigital.kz>
2019-07-17 20:37:21 +0600

    font roboto

--- src/containers/Layout/style.ts
+++ src/containers/Layout/style.ts
@@ -4,6 +4,9 @@ export default (theme: Theme) => {
   const headerAndFooterHeight = theme.layout.footerHeight + theme.layout.headerHeight;
 
   return {
+    root: {
+      fontFamily: 'roboto',
+    },
     content: {
       minHeight: `calc(100vh - ${headerAndFooterHeight}px)`,
       padding: theme.layout.spacing.l,
